2efc822523896617c3752f3658c57897d0a9405f
ASYA <asyailieva@yahoo.com>
2022-10-06 20:31:11 +0100

    Add new project Arrays - Exercise
    Add solution of problem from Arrays - Exercise.

---
+++ Arrays - Exercise/Train.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int[] people = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int currNumber = int.Parse(Console.ReadLine());
+
+                people [i] = currNumber;
+                sum += currNumber;
+                
+            }
+            Console.WriteLine(string.Join(" ", people));
+            Console.WriteLine(sum);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Programming-Fundamentals-C-sharp-Sept-2022
 This is a repository with solutions of problems in SoftUni Course: Programming Fundamentals with C# - September 2022.
-Basic Syntax - Exercise
+Basic Syntax - Exercise. Arrays - Exercise.

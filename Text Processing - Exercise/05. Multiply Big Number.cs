00ff19bef1a243645ed678dab4294a7ed706c92d
ASYA <asyailieva@yahoo.com>
2022-11-28 09:57:06 +0000

    Add task from Project Text Processing - Exercise.
    Add problem solutions from Project Text Processing - Exercise.

---
+++ Text Processing - Exercise/05. Multiply Big Number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Numerics;
+
+namespace _05._Multiply_Big_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger bigInt = BigInteger.Parse(Console.ReadLine());
+            int digit = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(bigInt * digit);
+        }
+    }
+}
